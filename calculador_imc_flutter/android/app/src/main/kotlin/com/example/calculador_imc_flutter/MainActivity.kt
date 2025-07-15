f2a9d1e04c395fb5cadccfa25c0512b9fafdd99f
Leozera2k <leofsousa98@gmail.com>
2025-07-15 14:19:22 -0300

    criando app

---
+++ calculador_imc_flutter/android/app/src/main/kotlin/com/example/calculador_imc_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.calculador_imc_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ calculador_imc_flutter/lib/services/my_app.dart
@@ -0,0 +1,17 @@
+import 'package:calculador_imc_flutter/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          appBarTheme: const AppBarTheme(color: Colors.blue)),
+      home: const HomePage(),
+    );
+  }
+}
